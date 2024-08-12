ce6f9cc21dfc92189c3ac69de7c91e158cd573fd
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 18:42:44 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,7 +1,23 @@
-public class Create{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load Values in Stack
-  // Iterate it by usig Iterator and While Loops
-}
-}
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class prac1 {
+        public static void main(String[]args){
+        // Create an Object for Stack
+          // Load Values in Stack
+          // Iterate it by usig Iterator and While Loops
+          Stack s=new Stack<>();
+          s.push(33);
+          s.push(587);
+          s.push(5566);
+          Enumeration f=s.elements();
+          Iterator d=s.iterator();
+          while (f.hasMoreElements()) {
+            System.out.println(f.nextElement());
+            
+          }
+
+        }
+        }
+
